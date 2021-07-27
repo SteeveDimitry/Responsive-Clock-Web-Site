8cdef2ef7e8632cf49949d702d100f3d1080d026
Steeve <steevedimitrynana98@gmail.com>
2021-07-27 19:42:48 +0100

    ..

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ var path = require('path');
 //app.use(express.static(__dirname)); // Current directory is root
 app.use(express.static(path.join(__dirname, 'public'))); //  "public" off of current is root
 
-app.listen(80);
+app.listen(8080);
 console.log('Listening on port 8080');
